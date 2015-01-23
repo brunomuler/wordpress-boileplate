9500d39bed1949493a15eea28ee1332c9e23b7ae
Bruno Muller <brunohmuller@gmail.com>
2015-01-23 13:21:58 -0200

    add local config

---
+++ local-config.php
@@ -0,0 +1,16 @@
+<?php
+//local-config
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
+
+define('WP_DEBUG', true);
+define('FS_METHOD', 'direct');
+define('WP_HOME', 'http://localhost/Sensorama/CatamoedaSite');
+define('WP_SITEURL', 'http://localhost/Sensorama/CatamoedaSite');
+// define('DB_NAME', 'catamoeda');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', 'admin');
+// define('DB_HOST', 'localhost');
+
+?>
